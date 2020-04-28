cf336baf7bb0f6b8730551662c8f51e63e3224b7
Chance Wright <cwright489@gmail.com>
2020-04-28 16:14:22 -0600

    heroku

--- routes/api.js
+++ routes/api.js
@@ -1,5 +1,5 @@
 const router = require("express").Router();
-const Workout = require("../models/workout.js");
+const Workout = require("../models/workoutQuery.js");
 console.log("api routes", Workout)
 
 router.post("/api/workouts", (req, res) => {
--- server.js
+++ server.js
@@ -13,7 +13,7 @@ app.use(express.json());
 
 app.use(express.static("public"));
 
-var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/workout"
+var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/cwright489"
 
 mongoose.connect(MONGODB_URI, {
   useNewUrlParser: true,
